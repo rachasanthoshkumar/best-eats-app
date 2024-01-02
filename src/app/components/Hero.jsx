a991a08052c2c336322b9bb28233b96f689ab106
rachasanthoshkumar <rachasanthosh2309@gmail.com>
2024-01-02 08:20:59 +0530

    resolved flex box error in navbar

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='max-w-[1680px] mx-auto p-2'>
+    <div className='container w-screen mx-auto p-2'>
         <div className='relative max-h-[500px] w-full'>
         <div className='absolute ml-1 text-white text-6xl w-full h-full font-bold sm:text-6xl lg:text-7xl flex flex-col justify-center'>
             <h2>The <span className='text-orange-400 '>Best</span></h2>
